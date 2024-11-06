4295b32f323458c761858b8a8b4faa4ee0eb788b
matudom <140403973+Krobertoo@users.noreply.github.com>
2024-11-06 17:14:15 +0100

    ver. 1
    segmentace obrazu pomocí pomocí thresholdingu a výpis jednotlivých snímků z videa

---
+++ segment.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+import os
+
+
+def segment_coins(image_path, threshold_value, min_contour_area, number_image):
+    # Načtení obrázku
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    if image is None:
+        print("Error: Unable to load image.")
+        return
+
+    # Převod do šedotónu
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Přidání Gaussova rozmazání pro snížení šumu fotograrie 
+    blurred_image = cv2.GaussianBlur(gray_image, (11, 11), 0)
+
+    # Binarizace obrázku s Gausovým rozmazáním
+    _, binary_mask = cv2.threshold(blurred_image, threshold_value, 255, cv2.THRESH_BINARY_INV)
+
+    # Nalezení obrysů v již binárním obrázku
+    contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Vytvoření složky pro uložení vyříznutých mincí
+    os.chdir(r"L:\CVUT\1roc\projekt1\seperatecoins") # Pro funkčnost přepsat cestu do složky s rozdělenými mincemi
+    os.makedirs(f"image{number_image}")
+    os.chdir(f"image{number_image}")
+
+    # Procházení všech obrysů a vyřezávání mincí
+    coin_count = 0
+    for i, contour in enumerate(contours):
+        # Ohlédnutí do zadané proměnné min_contour_area pro vyzaření falešných detekcí
+        if cv2.contourArea(contour) < min_contour_area:
+            continue
+
+        # Vykreslení masky pro danou minci
+        coin_mask = np.zeros_like(gray_image)
+        cv2.drawContours(coin_mask, [contour], -1, 255, thickness=cv2.FILLED)
+
+        # Aplikace masky na obrázek pro extrakci mince
+        segmented_coin = cv2.bitwise_and(image, image, mask=coin_mask)
+
+        # Vyříznutí mince z obrázku
+        x, y, w, h = cv2.boundingRect(contour)
+        cropped_coin = segmented_coin[y:y+h, x:x+w]
+
+        # Uložení vyříznuté mince
+        coin_count += 1
+        cv2.imwrite(f"coin_{coin_count}.png", cropped_coin)
+
+    print(f"Z obrázků segmentováno a uloženo {coin_count} mincí.\n")
+
+
+b=input("Kolik obrázků chcete segmentovat?\n")
+
+
+for j in range(int(b)):
+    os.chdir(r"L:\CVUT\1roc\projekt1") # Pro funkčnost přepsat cestu do složky s obrázky
+    print("Zadejte cestu k obrázku: ")
+    segment_coins(input(), 200, 500, j)
+    #print("Zadejte cestu k obrázku, hodnotu pro binarizaci (doporučená 200), minimální obrys mince (doporučená 500). [Vstupní hodnoty oddělujte enterem.]")
+    #segment_coins(input(), int(input()), int(input()), j)
+
+
+#segment_coins("testimage4.jpg", 200, 500, 1) # (200, 500 - testimage4.jpg) 
---
+++ video2frames.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+
+def extract_frames(video_path, output_folder):
+    # Vytvoření složky pro uložení snímků, pokud ještě neexistuje
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Načtení videa
+    video_capture = cv2.VideoCapture(video_path)
+    if not video_capture.isOpened(): # Kontrola, zda se video podařilo otevřít
+        print("Chyba: video se nepodařilo otevřít.")
+        return
+
+    frame_count = 0
+    while True:
+        # Přečtení dalšího snímku
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+
+        # Uložení sníkmku jako obrázek
+        frame_filename = os.path.join(output_folder, f"frame_{frame_count:04d}.png")
+        cv2.imwrite(frame_filename, frame)
+        frame_count += 1
+
+    video_capture.release()
+    print(f"Extracted {frame_count} frames and saved them to {output_folder}.")
+
+
+extract_frames("train_video.mp4", r"L:\CVUT\1roc\projekt1\frames")
